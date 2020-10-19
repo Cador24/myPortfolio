d3baffe82743797a30e551380c48db1cc3ade0f3
Marc R. Eclipse <marceclipse@gmail.com>
2020-10-19 17:40:04 -0400

    Added all sections; No logo; Before JS installs

---
+++ app.js
@@ -0,0 +1,20 @@
+const cl = console.log;
+
+cl("hello")
+
+
+let x = new Date();
+
+x.getFullYear();
+cl(x);
+
+
+
+/*Garbage collection;
+ES6 MODULES --- NATIVE import/export
+not supported by IE.
+
+Advantage: The browsers
+
+
+*/
