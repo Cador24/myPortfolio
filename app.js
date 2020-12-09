02adceae59e2c22439e8c7b31e460ff8ac71049d
Marc R. Eclipse <marceclipse@gmail.com>
2020-12-09 02:22:28 -0500

    v1.1: Started Modal - Z-index fixed; Logo, contact backend, and Waypoints required

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const cl = console.log;
 
 cl("hello")
 
+/* 
+1) click any Gallery => 
+  - Transparent Background <body>
+  - Freeze Y scroll or (Create Overflow wrapper div after body = hidden)
+  - Open Modal
+*/
 
-let x = new Date();
+// const bodyTag = document.querySelector('body')
+// const containerEl = document.querySelector('#modal-wrapper')
 
-x.getFullYear();
-cl(x);
+// // bodyTag.style.overflowY = "hidden";
+// containerEl.style.background = "black";
+// containerEl.style.zIndex = "200200";
 
+/* rgba(10, 10, 10, 0.849) */
+
+
+/* bodyTag.addEventListener('click', function(){
+  alert('hello...is it me you\'re looking for')
+}) */
