d2d2cf3933dd84872f6badca9bdfe3e6242632af
Marc R. Eclipse <marceclipse@gmail.com>
2020-12-13 14:26:24 -0500

    Site v1.1: Added modal - Design required; logo, contact backend, waypoints required.

--- app.js
+++ app.js
@@ -2,23 +2,39 @@ const cl = console.log;
 
 cl("hello")
 
+
+
+const bodyTag = document.querySelector('body')
+
+// const galleryEl2 = document.getElementById('gallery-container').children
+const modalWrapper = document.getElementById('modal-wrapper');
+const modalWindow = document.getElementById('modal-window');
+
+
 /* 
-1) click any Gallery => 
-  - Transparent Background <body>
-  - Freeze Y scroll or (Create Overflow wrapper div after body = hidden)
-  - Open Modal
-*/
+// for(let elem of galleryEl2) {
+//   elem.addEventListener('click', function() {
+//     alert('hello');
+//    });
+//   }; 
 
-// const bodyTag = document.querySelector('body')
-// const containerEl = document.querySelector('#modal-wrapper')
+/* Clicker Callback Function: */
 
-// // bodyTag.style.overflowY = "hidden";
-// containerEl.style.background = "black";
-// containerEl.style.zIndex = "200200";
+// Close Button Callback.
 
-/* rgba(10, 10, 10, 0.849) */
+ document.addEventListener('click', function(){
+    console.log(event.target.tagName);
+    if (event.target.tagName == 'IMG') {
+      modalWrapper.style.display = 'block'
+      modalWindow.style.display = 'block'
+      bodyTag.style.overflowY = "hidden";
 
+      // bodyTag.style.overflowY = "hidden";
+    }
+    if (event.target.id === 'modal-wrapper') {
+      modalWrapper.style.display = 'none'
+      modalWindow.style.display = 'none'
+      bodyTag.style.overflowY = "initial";
+    }
 
-/* bodyTag.addEventListener('click', function(){
-  alert('hello...is it me you\'re looking for')
-}) */
+  })
