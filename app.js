af385491651b0157cbeb89be7dcc02266f43bf2a
Marc R. Eclipse <marceclipse@gmail.com>
2020-12-15 23:25:40 -0500

    Site v1.1: Updated Modal partial design & functionality: 50% complete; logo, contact backend,and waypoints Required

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ cl("hello")
 const bodyTag = document.querySelector('body')
 
 // const galleryEl2 = document.getElementById('gallery-container').children
-const modalWrapper = document.getElementById('modal-wrapper');
+const modalMask = document.getElementById('modal-mask');
 const modalWindow = document.getElementById('modal-window');
 
 
@@ -23,18 +23,21 @@ const modalWindow = document.getElementById('modal-window');
 // Close Button Callback.
 
  document.addEventListener('click', function(){
-    console.log(event.target.tagName);
     if (event.target.tagName == 'IMG') {
-      modalWrapper.style.display = 'block'
+      modalMask.style.display = 'block'
       modalWindow.style.display = 'block'
       bodyTag.style.overflowY = "hidden";
-
-      // bodyTag.style.overflowY = "hidden";
     }
-    if (event.target.id === 'modal-wrapper') {
-      modalWrapper.style.display = 'none'
+    if (event.target.id === 'modal-mask') {
+      modalMask.style.display = 'none'
       modalWindow.style.display = 'none'
       bodyTag.style.overflowY = "initial";
     }
 
-  })
+  }) 
+
+
+
+  // modalMask.style.display = 'block'
+  // modalWindow.style.display = 'block'
+  // bodyTag.style.overflowY = "hidden";
